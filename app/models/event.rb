4fa1fa8edc4ef9860775c312d9d688f55945dd9b
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-19 13:59:36 -0500

    Add view for user
    Add view to show user created events.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@
 
 # Event model
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 end
