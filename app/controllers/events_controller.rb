2238b8b7e1e0afbf24bce192dccded7aa5a51ade
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-19 16:46:55 -0500

    Add event views
    Add event views to show its details.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,28 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date)
+  end
 end
