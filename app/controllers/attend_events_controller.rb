c96124868d3765464aef11a3f263d7534054bcc1
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-04-20 17:41:07 -0500

    Add attendance cancelation
    Add method to cancel attendance to an event.

--- app/controllers/attend_events_controller.rb
+++ app/controllers/attend_events_controller.rb
@@ -4,9 +4,15 @@
 class AttendEventsController < ApplicationController
   def create
     @event = Event.find(params[:event])
-
     @event.attendees.push(current_user)
 
     redirect_to @event
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.attendees.destroy(current_user)
+
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events
   resources :users
-  resources :attend_events, only: %i[create]
+  resources :attend_events, only: %i[create destroy]
 
   # Defines the root path route ("/")
   root 'events#index'
